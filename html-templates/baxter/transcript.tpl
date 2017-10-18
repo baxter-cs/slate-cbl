37980cb8cfa019fd7c207ac4df181ff157fb0596
halstedlarsson <halsted.larsson@gmail.com>
2017-10-18 01:12:11 -0400

    render classes for students

--- html-templates/baxter/transcript.tpl
+++ html-templates/baxter/transcript.tpl
@@ -1,17 +1,50 @@
 {extends designs/site.tpl}
 
 {block content}
-    <h3>Generate Transcript</h3>
-    <ul>
-    {foreach item=Student from=$students}
-        <li>
-            {$Student.name}  {$Student.id} {if $pretend}is eligible to graduate{else}has been graduated{/if} in the following competencies:
-            <ul>
-                {foreach item=competency from=$Student.competencies}
-                    <li>{$competency.currentLevel} &rarr; {$competency.nextLevel} in {$competency.code}</li>
-                {/foreach}
-            </ul>
-        </li>
-    {/foreach}
-    </ul>
+
+    {if $renderTranscript}
+        <h2>{$student->FullName|escape}</h2>
+        {foreach item=Competency from=$competencies}
+            <div>
+                <span>{$Competency.code}</span>
+                <span>{$Competency.currentLevel}</span>
+            </div>
+
+        {/foreach}
+
+    {elseif $renderProgress}
+        <h2>{$student->FullName|escape}</h2>
+        {foreach item=Section from=$courseSections}
+                <span>{$Section->Title}</span>
+        {/foreach}
+
+    {else}
+        <h3>Generate Transcript</h3>
+        <form method="POST">
+            {capture assign=term}
+                <select class="field-control inline medium" name="term">
+                    {foreach item=Term from=Slate\Term::getAll(array(order=array(ID=DESC)))}
+                        <option value="{$Term->ID|escape}">{$Term->Title|escape}</option>
+                    {/foreach}
+                </select>
+            {/capture}
+            {labeledField html=$term type=select label=Term class=auto-width}
+
+
+            {capture assign=studentsSelect}
+                <select class="field-control inline medium" name="studentID">
+                    <option value="">&ndash;select&ndash;</option>
+                        <optgroup label="My Sections">
+                            {foreach item=Student from=Emergence\People\Person::getAll(array(order=array(ID=DESC)))}
+                                <option value="{$Student->ID|escape}">{$Student->FullName|escape}</option>
+                            {/foreach}
+                        </optgroup>
+                </select>
+            {/capture}
+            {labeledField html=$studentsSelect type=select label=student class=auto-width}
+            <input type="submit" name="submitTranscript" value="Generate Transcript">
+            <input type="submit" name="submitProgress" value="Generate Progress Report">
+            </form>
+    {/if}
+
 {/block}
--- site-root/baxter/transcript.php
+++ site-root/baxter/transcript.php
@@ -1,41 +1,88 @@
 <?php
 
 use Slate\CBL\Competency;
+use Slate\CBL\StudentCompetency;
 use Slate\People\Student;
+use Slate\Courses\Section;
+use Slate\Courses\SectionParticipant;
 
 
 $GLOBALS['Session']->requireAccountLevel('Staff');
 
 
-$students = Student::getAllByWhere([
-    'Class' => Student::class
-]);
 $competencies = Competency::getAll();
-
 
 $transcriptStudents = [];
 $i = 0;
-foreach ($students as $student) {
+if ($_POST['submitTranscript']) {
+    $studentID = $_POST['studentID'];
+    $student = Student::getByID($studentID);
+
     $studentComps = [];
     foreach ($competencies as $Competency) {
-        $currentLevel = $Competency->getCurrentLevelForStudent($student);
-        $studentComps[] = [
-            'code' => $Competency->Code,
-            'currentLevel' => $currentLevel,
-            'nextLevel' => $currentLevel + 1            
-        ];        
+        $studentCompetencies = StudentCompetency::getAllByWhere([
+            'StudentID' => $student->ID,
+            'CompetencyID' => $Competency->ID
+        ]);
+        foreach ($studentCompetencies as $StudentCompetency) {
+          $studentComps[] = [
+              'code' => $Competency->Code,
+              'currentLevel' => $StudentCompetency->Level,
+              'created' => $StudentCompetency->Created
+          ];
+        }
     }
-    
-    $transcriptStudent = [
-        'name' => $student->FullName,
-        'id' => $student->ID,
-        'competencies' => $studentComps
-    ];
-    
-    $transcriptStudents[] = $transcriptStudent;
+    RequestHandler::respond('baxter/transcript', [
+        'student' => $student,
+        'competencies' => $studentComps,
+        'renderTranscript' => true
+    ]);
+
+} elseif ($_POST['submitProgress']) {
+    $studentID = $_POST['studentID'];
+    $termID = $_POST['termID'];
+    $student = Student::getByID($studentID);
+    $sectionParticipants = SectionParticipant::getAllByWhere([
+       'PersonID' => $studentID,
+    ]);
+
+    $courseSections = [];
+
+    foreach ($sectionParticipants as $SectionParticipant){
+        $section = $SectionParticipant->Section;
+        #if($section->TermID == $termID) {
+        $courseSections[] = $section;
+        #}
+    }
+
+
+    RequestHandler::respond('baxter/transcript', [
+        'student' => $student,
+        'courseSections' => $courseSections,
+        'renderProgress' => true
+
+    ]);
+
+}  else {
+    $students = Student::getAllByWhere([
+        'Class' => Student::class
+    ]);
+
+    foreach ($students as $student) {
+        $studentLevels = [];
+
+        $transcriptStudent = [
+            'name' => $student->FullName,
+            'id' => $student->ID,
+            'competencies' => $studentComps
+        ];
+
+        $transcriptStudents[] = $transcriptStudent;
+    }
+
+
+
+    RequestHandler::respond('baxter/transcript', [
+        'students' => $transcriptStudents
+    ]);
 }
-
-
-RequestHandler::respond('transcript', [
-    'students' => $transcriptStudents
-]);
